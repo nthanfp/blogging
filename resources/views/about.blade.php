dd5e7ee77939cb7e4a0aff28ddb39100574d9c1e
Nathanael Ferry Pratama <ntnferry1505@gmail.com>
2023-09-16 13:48:27 +0700

    Add About

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.template')
+
+@section('title', 'About')
+
+@section('content')
+<div class="mt-4 p-5 bg-primary text-white rounded">
+    <h1>{{ $title }}</h1>
+    <p>{{ $description }}</p>
+</div>
+@endsection
