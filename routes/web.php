df096e96fa7ecf4d74781766910ee5b20db166ad
Nathanael Ferry Pratama <ntnferry1505@gmail.com>
2023-09-16 13:48:48 +0700

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('landing');
+// Route::get('/', function () {
+//     return view('landing');
+// });
+
+Route::view('/', 'landing');
+
+Route::get('/about', function(){
+    $title = 'About Us';
+    $description = 'Blogging is website for sharing your thoughts and ideas with.';
+
+   return view('about', compact('title', 'description'));
 });
