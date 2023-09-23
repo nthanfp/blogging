944c41bf34f79880211479fc3181902bd0333d33
Nathanael Ferry Pratama <ntnferry1505@gmail.com>
2023-09-23 14:15:44 +0700

    Update Web Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', LandingController::class);
 
 Route::get('/about', [AboutController::class, 'index']);
+
+Route::get('/contact-us', [ContactController::class, 'index']);
+
+Route::post('/contact-us', [ContactController::class, 'store']);
