93121e314936b6934ce15cd8baa1a5147362a175
Nathanael Ferry P <ntnferry1505@gmail.com>
2023-10-02 10:08:42 +0700

    Add Articles CRUD

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|min:3|max:255',
+            'body' => 'required|string',
+        ]);
+
+        // Simpan gambar jika ada
+        if ($request->hasFile('image')) {
+            // Validasi gambar
+            $request->validate([
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            ]);
+            // Upload gambar dan dapatkan path gambar yang diupload
+            $imagePath = $request->file('image')->store('public/images');
+
+            $validated['image'] = $imagePath;
+        }
+
+        // Buat artikel baru
+        $article = Article::create([
+            'title' => $validated['title'],
+            'body' => $validated['body'],
+            'published_at' => $request->has('is_published') ? Carbon::now() : false,
+            'image' => $validated['image'] ?? null,
+        ]);
+
+        return $article;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Article $article)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Article $article)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\LandingController;
+use App\Models\Article;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +28,5 @@ Route::get('/about', [AboutController::class, 'index']);
 
 Route::get('/contact-us', [ContactController::class, 'index'])->name('contact-us.index');
 Route::post('/contact-us', [ContactController::class, 'store'])->name('contact-us.store');
+
+Route::resource('articles', ArticleController::class);
